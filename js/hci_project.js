54bdce4a0f688146e7e425538dd27451498ee520
Zarcen Chen <zarcen@gmail.com>
2014-11-03 14:35:28 -0600

    finished fluid/fixed page and added hci_project.js

--- js/hci_project.js
+++ js/hci_project.js
@@ -72,8 +72,8 @@ function refreshContactWhenFlip_Fixed() {
   //alert($('#article')[0].style.fontSize);
   if(w > 600){ // horizontal orientation
     $('#article')[0].style.fontSize = '28px';
-    $('#container')[0].style.marginLeft = '8px';
-    $('#container')[0].style.marginRight = '8px';
+    $('#container')[0].style.marginLeft = '10px';
+    $('#container')[0].style.marginRight = '10px';
   }
   else if (w < 400) { // vertical orientation
     $('#article')[0].style.fontSize = '16px';
